adbeea0e9fadc81f9c130028bbc6ab9c58d95c96
zdx-1 <19546328912@163.com>
2025-04-11 00:10:04 +0800

    创建会话图标组件npm install react-icons --save

--- video-chat/video-chat-frontend/src/pages/Dashboard/components/DirectCall/DirectCall.js
+++ video-chat/video-chat-frontend/src/pages/Dashboard/components/DirectCall/DirectCall.js
@@ -6,45 +6,47 @@ import IncomingCallDialog from '../IncomingCallDialog/IncomingCallDialog';
 import LocalVideoView from '../LocalVideoView/LocalVideoView';
 import RemoteVideoView from '../RemoteVideoView/RemoteVideoView';
 import {
-  callStates,
-  setCallRejected,
+    callStates,
+    setCallRejected,
 } from '../../../../store/actions/callActions';
+import ConversationButtons from '../ConversationButtons/ConversationButtons';
 const DirectCall = ({
-                      localStream,
-                      remoteStream,
-                      callerUsername,
-                      callState,
-                      callingDialogVisible,
-                      callRejected,
-                      hideCallRejectedDialog,
+                        localStream,
+                        remoteStream,
+                        callerUsername,
+                        callState,
+                        callingDialogVisible,
+                        callRejected,
+                        hideCallRejectedDialog,
                     }) => {
-  return (
-      <>
-        <LocalVideoView localStream={localStream} />
-        {remoteStream && <RemoteVideoView remoteStream={remoteStream} />}
-        {/* 不同状态下的直接呼叫对话框 */}
-        {callRejected.rejected && (
-            <CallRejectDialog
-                reason={callRejected.reason}
-                hideCallRejectedDialog={hideCallRejectedDialog}
-            />
-        )}
-        {callState === callStates.CALL_REQUESTED && (
-            <IncomingCallDialog callerUsername={callerUsername} />
-        )}
-        {callingDialogVisible && <CallingDialog />}
-      </>
-  );
+    return (
+        <>
+            <LocalVideoView localStream={localStream} />
+            {remoteStream && <RemoteVideoView remoteStream={remoteStream} />}
+            {/* 不同状态下的直接呼叫对话框 */}
+            {callRejected.rejected && (
+                <CallRejectDialog
+                    reason={callRejected.reason}
+                    hideCallRejectedDialog={hideCallRejectedDialog}
+                />
+            )}
+            {callState === callStates.CALL_REQUESTED && (
+                <IncomingCallDialog callerUsername={callerUsername} />
+            )}
+            {callingDialogVisible && <CallingDialog />}
+            <ConversationButtons />
+        </>
+    );
 };
 const mapStateToProps = ({ call }) => {
-  return { ...call };
+    return { ...call };
 };
 
 const mapDispatchToProps = (dispatch) => {
-  return {
-    hideCallRejectedDialog: (callRejectedDetails) =>
-        dispatch(setCallRejected(callRejectedDetails)),
-  };
+    return {
+        hideCallRejectedDialog: (callRejectedDetails) =>
+            dispatch(setCallRejected(callRejectedDetails)),
+    };
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(DirectCall);
