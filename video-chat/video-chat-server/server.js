5949633fe9d9e308d8f47dbec04a6726ab4fd4f0
zdx-1 <19546328912@163.com>
2025-04-10 10:09:45 +0800

    断开连接时移除存储在服务器中的用户

--- video-chat/video-chat-server/server.js
+++ video-chat/video-chat-server/server.js
@@ -25,7 +25,7 @@ const io = socket(server, {
 });
 
 //初始化对等连接用户数组
-const peers = [];
+let peers = [];
 
 //定义广播类型的常量
 const broadcastEventTypes = {
@@ -36,7 +36,8 @@ const broadcastEventTypes = {
 //监听客户端socket连接
 io.on('connection', (socket) => {
   socket.emit('connection', null);
-  console.log('新用户加入房间:'+socket.id);
+  console.log('新用户加入房间');
+  console.log(socket.id);
 
   //服务器保存注册的新用户数据
   socket.on('register-new-user', (data) => {
@@ -48,6 +49,16 @@ io.on('connection', (socket) => {
     console.log(peers);
 
     //向所有连接到客户端用户广播，并发送活跃用户列表
+    io.sockets.emit('broadcast', {
+      event: broadcastEventTypes.ACTIVE_USERS,
+      activeUsers: peers,
+    });
+  });
+
+  //断开连接时移除存储在服务器的用户并向其他客户端进行广播
+  socket.on('disconnect', () => {
+    console.log('有用户下线了');
+    peers = peers.filter((peer) => peer.socketId !== socket.id);
     io.sockets.emit('broadcast', {
       event: broadcastEventTypes.ACTIVE_USERS,
       activeUsers: peers,
