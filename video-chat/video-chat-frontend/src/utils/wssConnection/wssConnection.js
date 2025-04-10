540f426ab0e70be3e1a2b9c6c3c3a17f2513799b
zdx-1 <19546328912@163.com>
2025-04-10 22:46:42 +0800

    处理预呼叫和准备预呼叫回复

--- video-chat/video-chat-frontend/src/utils/wssConnection/wssConnection.js
+++ video-chat/video-chat-frontend/src/utils/wssConnection/wssConnection.js
@@ -29,6 +29,11 @@ export const connectWithSocket = () => {
   socket.on('pre-offer', (data) => {
     webRTCHandler.handlePreOffer(data);
   });
+
+  //呼叫方监听服务器返回的应答方回复的data数据
+  socket.on('pre-offer-answer', (data) => {
+    webRTCHandler.handlePreOfferAnswer(data);
+  });
 };
 
 //注册新用户
@@ -45,7 +50,7 @@ const handleBroadcastEvents = (data) => {
     case broadcastEventTypes.ACTIVE_USERS:
       // 过滤客户端本身的信息
       const activeUsers = data.activeUsers.filter(
-        (activeUser) => activeUser.socketId !== socket.id
+          (activeUser) => activeUser.socketId !== socket.id
       );
       // 派发action，保存活跃用户
       store.dispatch(dashboardActions.setActiveUsers(activeUsers));
@@ -58,4 +63,9 @@ const handleBroadcastEvents = (data) => {
 //向服务器发送预呼叫数据
 export const sendPreOffer = (data) => {
   socket.emit('pre-offer', data);
+};
+
+//应答方向服务器发送预呼叫处理的回复
+export const sendPreOfferAnswer = (data) => {
+  socket.emit('pre-offer-answer', data);
 };
